4f5f10de8bc5b1797b7c6a40d8f60588e48635af
korolev_fk <korolev_fk@deltag.ru>
2025-07-15 12:06:41 +0300

    Added launch file which contains the functions that launches all plots visualizations sequentially and forms list from them for future actions

---
+++ src/tech/launch.py
@@ -0,0 +1,37 @@
+from collections.abc import Callable
+from typing import List, Tuple
+import pandas as pd
+import datetime
+import numpy as np
+from io import BytesIO
+
+
+def launch_plot_functions(functions: List[Callable],
+                          df: pd.DataFrame, metadata: pd.DataFrame, trainings: pd.DataFrame
+                          ) -> List[Tuple[BytesIO, str]]:
+    """Sequentially launches all plots making functions
+
+    Args:
+        functions (List[Callable]): function that is need to be called
+        df (pd.DataFrame): master dataframe with all data
+        metadata (pd.DataFrame): metadata of master dataframe that neccesary must contain metatype and feature
+        trainings (pd.DataFrame): trainings dataframe, which must containt date column
+
+    Returns:
+        List[Tuple[BytesIO, str]]: list of tuples where each one contains BytesIO view of plot firstly, and
+                str message to this plot secondly
+    """
+    # assignment of current week, previous week and previous period. Current week is counted from now and 7 days before,
+    # previous week 7 days before current accordingly. Is needed for many plots.
+    df['period'] = np.where(
+        df['date'] > (datetime.datetime.now() - datetime.timedelta(days=8)).date(),
+        'Current week',
+        np.where(
+            (df['date'] > (datetime.datetime.now() - datetime.timedelta(days=15)).date()) &
+            (df['date'] <= (datetime.datetime.now() - datetime.timedelta(days=8)).date()),
+            'Previous week',
+            'Previous period'
+        )
+    )
+
+    return [f(df, metadata) for f in functions]
