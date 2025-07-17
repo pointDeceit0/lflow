8137eeba381f3930fc0d0eb1227c91767280d68b
korolev_fk <korolev_fk@deltag.ru>
2025-07-17 11:22:42 +0300

    Main launching transfered to main function

--- src/main.py
+++ src/main.py
@@ -13,13 +13,37 @@ import tech.sending as snd
 kwargs = {
     "habbits_radar": {'exclude': ('creatine', 'was_in_university')},
     "habbits_linear": {'grannulation': 'week',
-                       'exclude': ('creatine', 'was_in_university')},
+                       'exclude': ('creatine', 'was_in_university'),
+                       'number_of_weeks': 10},
     "nutrition_heatmap": None,
 }
 
 unions = {
     'frequencies': ['habbits_radar', 'habbits_linear']
 }
+
+
+def main(CRED_FILE_PATH, SERVICES, SPREADSHEET_ID):
+    print('-------------------------------------')
+    start1 = time.time()
+    resource = con.extract_data(CRED_FILE_PATH, SERVICES)
+    t1 = time.time() - start1
+    print(f'---> Data was extracted for {round(t1, 2)} s.')
+    start2 = time.time()
+    df, metadata, trains = dt.transform_enrichment_data(resource, SPREADSHEET_ID)
+    t2 = time.time() - start2
+    print(f'---> Data was transformed for {round(t2, 2)} s.')
+    start3 = time.time()
+    snd.send_messages(
+        bot_token,
+        chat_id,
+        launch.launch_plot_functions(functions, df, metadata, trains, **kwargs),
+        functions
+    )
+    t3 = time.time() - start3
+    print(f'---> Report was sent for {round(t3, 2)} s.\n')
+    print(f'Total execution time is {round(t1 + t2 + t3, 2)} s.')
+    print('-------------------------------------')
 
 
 if __name__ == '__main__':
@@ -47,28 +71,6 @@ if __name__ == '__main__':
     print()
 
     if functions:
-        print('-------------------------------------')
-        start1 = time.time()
-        resource = con.extract_data(CRED_FILE_PATH, SERVICES)
-        t1 = time.time() - start1
-        print(f'---> Data was extracted for {round(t1, 2)} s.')
-
-        start2 = time.time()
-        df, metadata, trains = dt.transform_enrichment_data(resource, SPREADSHEET_ID)
-        t2 = time.time() - start2
-        print(f'---> Data was transformed for {round(t2, 2)} s.')
-
-        start3 = time.time()
-        snd.send_messages(
-            bot_token,
-            chat_id,
-            launch.launch_plot_functions(functions, df, metadata, trains, **kwargs),
-            functions
-        )
-        t3 = time.time() - start3
-        print(f'---> Report was sent for {round(t3, 2)} s.\n')
-
-        print(f'Total execution time is {round(t1 + t2 + t3, 2)} s.')
-        print('-------------------------------------')
+        main(CRED_FILE_PATH, SERVICES, SPREADSHEET_ID)
     else:
         print('No valid functions recieved.')
